d1b8b44bb52c784948707ad426b5b5dffe01a051
lonlin0731 <lonlin0731@163.com>
2016-10-15 23:45:26 +0800

    init

---
+++ assets/js/main.js
@@ -0,0 +1,70 @@
+---
+layout: null
+---
+
+$(function() {
+  var toc     = $('.toc-link'),
+      sidebar = $('#sidebar'),
+      main    = $('#main'),
+      menu    = $('#menu'),
+      x1, y1;
+
+  // run this function after pjax load.
+  var afterPjax = function() {
+    // open links in new tab.
+    $('#main').find('a').filter(function() {
+      return this.hostname != window.location.hostname;
+    }).attr('target', '_blank');
+
+    // your scripts
+  };
+  afterPjax();
+
+
+  // NProgress
+  NProgress.configure({ showSpinner: false });
+
+
+  // Pjax
+  $(document).pjax('#sidebar-avatar, .toc-link', '#main', {
+    fragment: '#main',
+    timeout: 3000
+  });
+
+  $(document).on({
+    'pjax:click': function() {
+      NProgress.start();
+      main.removeClass('fadeIn');
+    },
+    'pjax:end': function() {
+      afterPjax();
+      NProgress.done();
+      main.scrollTop(0).addClass('fadeIn');
+      menu.add(sidebar).removeClass('open');
+      {% if site.google_analytics %}
+      ga('set', 'location', window.location.href);
+      ga('send', 'pageview');
+      {% endif %}
+    }
+  });
+
+
+  // Tags Filter
+  $('#sidebar-tags').on('click', '.sidebar-tag', function() {
+    var filter = $(this).data('filter');
+    if (filter === 'all') {
+      toc.fadeIn(350);
+    } else {
+      toc.hide();
+      $('.toc-link[data-tags~=' + filter + ']').fadeIn(350);
+    }
+    $(this).addClass('active').siblings().removeClass('active');
+  });
+
+
+  // Menu
+  menu.on('click', function() {
+    $(this).add(sidebar).toggleClass('open');
+  });
+
+});
